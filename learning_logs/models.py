e7ac44167eba4ad3ae322b1aeed58dfa05468898
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-07-08 18:18:18 -0700

    Initial Commit

---
+++ learning_logs/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """User will supply information needed to learn about"""
+    text = models.CharField(max_length = 200)
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.text
+
+class Entry(models.Model):
+    #Specific things that need to be learned about a topic using many to one topic:
+    topic = models.ForeignKey(Topic)
+    text = models
+
