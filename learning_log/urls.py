a295a287fc20d64f5d88ab1eb3fbb9de450089de
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-07-16 12:05:24 -0700

    Added webpage where information will be portrayed

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+app_name = "learning_logs"
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('learning_logs.urls'))
 ]
+
+
---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+#Need to define url patterns for learning_logs:
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #This is the home page
+    path('', views.index, name = "index")
+]
+
