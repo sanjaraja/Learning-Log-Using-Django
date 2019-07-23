013f61c420693210aa6553ff4b2cfd981b873d4a
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-07-23 14:15:19 -0700

    Added feature where login page will now show

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-app_name = "learning_logs"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include("users.urls")),
